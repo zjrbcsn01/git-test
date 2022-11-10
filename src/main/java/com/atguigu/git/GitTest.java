75fa74bafa2696f6cae2cb5e182f64acf7ff302f
zjr <zjr@atguigu.com>
2022-11-10 12:34:59 +0800

    master commit06

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello hot-fix...");
         System.out.println("hello hot-fix second test...");
         System.out.println("hello master test123...");
+        System.out.println("hello master test456...");
 
     }
 }
